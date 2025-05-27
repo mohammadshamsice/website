6ddbd93a1be2174329c7bc6236874f614841289e
{mohammadshamsice} <mohammadshamsi.pc@gmail.com>
2025-05-27 06:01:32 +0330

    edit Pipe project

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ projects = {
         "progress": 50,
         "estimated_completion": "25 July 2025"
     },
-    "Pipe": {
+    "pipe": {
         "title": "Pipe rehabilitation with FRP",
         "status": "In Progress",
         "progress": 90,
